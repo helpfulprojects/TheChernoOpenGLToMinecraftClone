a509798e9f6c7bc9d316aa57c1159164753b48be
helpfulprojects <helpfulprojectsgithub@gmail.com>
2025-03-21 11:16:20 +0200

    add anisotropic filtering to remove the far away fuzzy look

--- TheChernoOpenGL/src/Texture.cpp
+++ TheChernoOpenGL/src/Texture.cpp
@@ -2,6 +2,8 @@
 #include "stb_image/stb_image.h"
 #include <GL\glew.h>
 #include "ErrorManager.h"
+#include <iostream>
+#include <cmath>
 
 const float Texture::m_Offset = (float)(Texture::m_AtlasSize / (float)Texture::m_TexturesPerRow) / (float)Texture::m_AtlasSize;
 
@@ -16,8 +18,18 @@ Texture::Texture(const std::string& filepath): m_RendererID(0), m_Filepath(filep
 
 	GlCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_LocalBuffer));
 	GlCall(glGenerateMipmap(GL_TEXTURE_2D));
-	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
 	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+	GlCall(glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, 0));
+	if (glewIsSupported("GL_EXT_texture_filter_anisotropic") || GLEW_EXT_texture_filter_anisotropic) {
+		std::cout << "Anisotropic filtering supported" << std::endl;
+		GLfloat aniso = 0.0f;
+		glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY, &aniso);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY, std::min(4.0f,aniso));
+	}
+	else {
+		std::cout << "Anisotropic filtering not supported" << std::endl;
+	}
 	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
 	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 
